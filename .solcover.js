7581487fa3378bab22594d77271f4b216ba77b37
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-28 16:40:29 +0530

    coverage fix

--- .solcover.js
+++ .solcover.js
@@ -2,5 +2,5 @@ module.exports = {
   //   port: 8555,
   //   testCommand: "truffle test --network coverage",
   //   compileCommand: "truffle compile --network coverage",
-  copyPackages: ["openzeppelin-solidity"]
+  copyPackages: ["openzeppelin-solidity", "electusaction", "electusvoting", "electusprotocol"]
 };

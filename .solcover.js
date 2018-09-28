b5ff697ca92aeabd7f966cfa8466cf52983ecd18
chaitanya potti <chaitanya.potti@gmail.com>
2018-09-28 16:51:58 +0530

    solcover options change

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,9 @@ module.exports = {
   //   port: 8555,
   //   testCommand: "truffle test --network coverage",
   //   compileCommand: "truffle compile --network coverage",
-  testrpcOptions: "--defaultBalanceEther=1000 --accounts=20",
+  norpc: true,
+  port: 8555,
+  testrpcOptions: "-p 8555 --defaultBalanceEther=1000",
+  accounts: 20,
   copyPackages: ["openzeppelin-solidity", "electusaction", "electusvoting", "electusprotocol"]
 };

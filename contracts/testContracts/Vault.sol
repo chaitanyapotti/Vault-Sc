458fef60ab0e1b57dc6fdfce9d677f68894d995a
chaitanya potti <chaitanya.potti@gmail.com>
2018-10-22 12:50:23 +0530

    updated vault contract and package version

--- contracts/testContracts/Vault.sol
+++ contracts/testContracts/Vault.sol
@@ -4,16 +4,6 @@ import "electusprotocol/contracts/ERC1261MetaData.sol";
 
 
 contract Vault is ERC1261MetaData {
-    struct PendingRequest {
-        bool isPending;
-        uint[] attributes;
-    }
-
-    event ApprovedMembership(address indexed _user);
-    event RequestedMembership(address indexed _user);
-
-    mapping(address => PendingRequest) public pendingRequests;
-
     uint public fee;
 
     constructor(bytes32 _orgName, bytes32 _orgSymbol, uint _fee) public 
@@ -26,29 +16,14 @@ contract Vault is ERC1261MetaData {
     }
 
     function requestMembership(uint[] _attributeIndexes) external payable {
-        require(!isCurrentMember(msg.sender), "Already a member");
         //Do some checks before assigning membership
         require(msg.value > fee, "Not enough ether sent for membership");
+        require(!isCurrentMember(msg.sender), "Already a member");
         PendingRequest storage request = pendingRequests[msg.sender];
         request.isPending = true;
         request.attributes = _attributeIndexes;
-        address admin = owner();
         emit RequestedMembership(msg.sender);
+        address admin = owner();
         admin.transfer(msg.value);
-    }
-
-    function approveRequest(address _user) external onlyOwner {
-        PendingRequest storage request = pendingRequests[_user];
-        require(request.isPending, "Hasn't sent ether yet");
-        super._assign(_user, request.attributes);
-        emit ApprovedMembership(_user);
-    }
-
-    function discardRequest(address _user) external onlyOwner {
-        PendingRequest storage request = pendingRequests[_user];
-        require(request.isPending, "Hasn't sent ether yet");
-        //what's preventing us from collecting ether from users and discarding all requests
-        request.isPending = false;
-        delete request.attributes;
     }
 }
